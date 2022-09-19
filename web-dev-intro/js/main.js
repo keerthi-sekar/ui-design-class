a6ee7da02edf28308480e4220d6a0c476c85568b
Keerthi Sekar <sekarku@mail.uc.edu>
2022-09-19 14:23:19 -0400

    start jss

--- web-dev-intro/js/main.js
+++ web-dev-intro/js/main.js
@@ -1,1 +1,19 @@
 console.log("Hello!  This is your javascript file."); 
+
+IncreaseTemperature()
+{
+    switch(id_name)
+    {
+        case "fridge_value":
+            let fridge_value = document.getElementById('fridge_value').value;
+            fridge_value++;
+            break;
+        case "":
+            break;
+        case "":
+            break;
+        default:
+
+    }
+
+}

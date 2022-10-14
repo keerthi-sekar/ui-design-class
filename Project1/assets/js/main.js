5707a3fe140ddf72f812044f608475cb1976c728
Keerth Sekar <keerthi.sekar101@gmail.com>
2022-10-14 16:26:24 -0400

    Check stock with items in db

--- Project1/assets/js/main.js
+++ Project1/assets/js/main.js
@@ -12,17 +12,55 @@ var item1 = document.getElementById('1');
 var item2 = document.getElementById('2');
 var item3 = document.getElementById('3');
 var item4 = document.getElementById('4');
-var fileInput = document.getElementById('file_input');
 var verify = document.getElementById('status_button');
 var statusCheck = document.getElementById('status_check');
 var transferList = document.getElementById('transfer_list');
+const input = document.querySelector('file_input');
+const shoppingList = [item1, item2, item3, item4];
 
 function StatusButtonClick(){
-    //making assumption that all items are in stock in list for concept
-    item1.textContent = input1.value + ' - In Stock';
-    item2.textContent = input2.value + ' - In Stock';
-    item3.textContent = input3.value + ' - In Stock';
-    item4.textContent = input4.value + ' - In Stock';
+    var parsed1 = input1.value.toLowerCase();
+    var parsed2 = input2.value.toLowerCase();
+    var parsed3 = input3.value.toLowerCase();
+    var parsed4 = input4.value.toLowerCase();
+    
+    if(parsed1 == "icecream" || parsed1 == "grapes" || parsed1 == "salmon" || parsed1 == "flowers")
+    {
+        console.log(parsed1);
+        item1.textContent = input1.value + ' - In Stock';
+    }
+    else
+    {
+        item1.textContent = input1.value + ' - Not in Stock';
+    }
+    
+    if(parsed2 == "icecream" || parsed2 == "grapes" || parsed2 == "salmon" || parsed2 == "flowers")
+    {
+        item2.textContent = input2.value + ' - In Stock';
+    }
+    else
+    {
+        item2.textContent = input2.value + ' - Not Available';
+    }
+    
+    if(parsed3 == "icecream" || parsed3 == "grapes" || parsed3 == "salmon" || parsed3 == "flowers")
+    {
+        item3.textContent = input3.value + ' - In Stock';
+    }
+    else
+    {
+        item3.textContent = input3.value + ' - Not Available';
+    }
+    
+    if(parsed4 == "icecream" || parsed4 == "grapes" || parsed4 == "salmon" || parsed4 == "flowers")
+    {
+        item4.textContent = input4.value + ' - In Stock';
+    }
+    else
+    {
+        item4.textContent = input4.value + ' - Not Available';
+    }
+
     status_check.textContent = 'Status: Checked';
 }
 
